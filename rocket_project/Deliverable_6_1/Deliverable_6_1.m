483d011e0ea2f2159e342aec4a69d97d090f1bcc
alifuatsahin <alifuatpc@gmail.com>
2023-12-25 21:18:30 +0100

    6.2 finalized

--- rocket_project/Deliverable_6_1/Deliverable_6_1.m
+++ rocket_project/Deliverable_6_1/Deliverable_6_1.m
@@ -11,12 +11,12 @@ H = 20*Ts; % Horizon length in seconds
 nmpc = NmpcControl(rocket, H);
 
 % MPC reference with default maximum roll = 15deg
-ref = [2,2,2,deg2rad(40)]';
-% ref = @(t_, x_) refTVC(t_); 
+% ref = [2,2,2,deg2rad(40)]';
+% ref = @(t_, x_) ref_TVC(t_); 
 
 % MPC reference with specified maximum roll = 50deg 
-% rollmax = deg2rad(50); 
-% ref = @(t_, x_) refTVC(t, rollmax); 
+rollmax = deg2rad(50); 
+ref = @(t_, x_) ref_TVC(t_/2, rollmax); 
 
 % Evaluate once and plot optimal open−loop trajectory, 
 % pad last input to get consistent size with time and state 
@@ -24,6 +24,7 @@ ref = [2,2,2,deg2rad(40)]';
 % U_opt(:,end+1) = nan; 
 % ph = rocket.plotvis(T_opt, X_opt, U_opt, ref); 
 
-Tf = 60; 
+Tf = 60;
+rocket.anim_rate = 3;
 [T,X,U,Ref] = rocket.simulate(x0,Tf, @nmpc.get_u, ref);
 ph = rocket.plotvis(T, X, U, Ref);
