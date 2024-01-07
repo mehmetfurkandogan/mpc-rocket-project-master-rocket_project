f0f68ed9ca843197b2cddfc04e6ebffe8d3b7547
alifuatsahin <alifuatpc@gmail.com>
2024-01-07 17:37:14 +0100

    selam

--- rocket_project/ToDo/td21.m
+++
@@ -1,9 +0,0 @@
-clc;
-clear;
-
-Ts = 1/20;
-
-rocket= Rocket(Ts);
-
-[xs,us] = rocket.trim(); % Compute steady−state for which 0 = f(xs,us) 
-sys = rocket.linearize(xs,us); % Linearize the non linear model about trim point
