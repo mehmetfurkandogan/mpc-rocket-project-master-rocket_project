97b5c2010defdceb9f4488e8c964919eeefce5c4
alifuatsahin <alifuatpc@gmail.com>
2024-01-07 16:14:52 +0100

    Update Deliverable_6_2.m

--- rocket_project/Deliverable_6_2/Deliverable_6_2.m
+++ rocket_project/Deliverable_6_2/Deliverable_6_2.m
@@ -3,6 +3,7 @@ addpath(fullfile('..', 'src'));
 close all; clear; clc;
 
 %% TODO: This file should produce all the plots for the deliverable
+% Compansated
 Ts = 1/40; 
 rocket = Rocket(Ts);
 expected_delay = 5;
@@ -16,5 +17,21 @@ rocket.mass = 1.75;
 rocket.delay = delay; 
 [T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
 ph = rocket.plotvis(T, X, U, Ref);
-exportgraphics(ph.fig,'plots/delay_compansated_nmpc.eps', BackgroundColor='none',ContentType='vector')
+exportgraphics(ph.fig,'plots/delay_compansated_nmpc_corrected.eps', BackgroundColor='none',ContentType='vector')
+
+%% Partially Compansated
+Ts = 1/40; 
+rocket = Rocket(Ts);
+expected_delay = 3;
+delay = 5;
+H = 20*Ts;
+nmpc = NmpcControl(rocket, H, expected_delay);
+x0 = zeros(12, 1);
+ref = [0.5, 0, 1, deg2rad(65)]';
+Tf = 10;
+rocket.mass = 1.75;
+rocket.delay = delay; 
+[T, X, U, Ref] = rocket.simulate(x0, Tf, @nmpc.get_u, ref);
+ph = rocket.plotvis(T, X, U, Ref);
+exportgraphics(ph.fig,'plots/delay_compansated_nmpc_partial.eps', BackgroundColor='none',ContentType='vector')
 
